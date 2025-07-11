92aa3119f4714662e111818122c250bc26fdfcdc
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-07-11 15:34:50 +0530

    basic signup function

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ class devbook:
                             4. Press 4 to message a friend
                             5. Press any other key to exit""")
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
             pass
         elif user_input == "3":
@@ -22,6 +22,15 @@ class devbook:
             pass
         else:
             exit()
+
+    def signup(self):
+        email = input("Enter your email here -> ")
+        pwd = input("Enter your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("You have successfully signed up!")
+        print("\n")
+        self.menu()
 
 
 obj = devbook()
